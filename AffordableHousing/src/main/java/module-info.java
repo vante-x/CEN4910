670aa066734b93f37df0df7cd0453d0924ac4347
Joanna Smith <joannanole@gmail.com>
2022-05-24 09:09:40 -0400

    updated latest changes to requirements

--- AffordableHousing/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module orlando.affordablehousing {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens orlando.affordablehousing to javafx.fxml;
-    exports orlando.affordablehousing;
-}
