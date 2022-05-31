4e6e47688624f865178b3f9ad554d23ee1b628a1
Joanna Smith <joannanole@gmail.com>
2022-05-31 14:09:12 -0400

    Issue #9
    Added APIs
    added shared module src/shared/components/google-maps/map.component
    added <app-google-map></app-google-map> to contact page.

--- Affordable-Housing/src/app/contact/contact.module.ts
+++ Affordable-Housing/src/app/contact/contact.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
 import { SharedModule } from '../shared/shared.module';
-import { GoogleLocationMapComponent } from './google-location-map/google-location-map.component';
+import { AppComponent  } from '../shared/components/google-maps/map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [ContactComponent, GoogleLocationMapComponent],
+  declarations: [ContactComponent, AppComponent ],
   imports: [CommonModule, ContactRoutingModule, SharedModule, GoogleMapsModule, HttpClientJsonpModule],
-  exports: [GoogleLocationMapComponent]
+  exports: [AppComponent]
 })
 export class ContactModule {}
