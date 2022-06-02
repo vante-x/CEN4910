8f7f23029b92aaeda040a9753f6895932218f284
Alayan2 <90277439+Alayan2@users.noreply.github.com>
2022-06-02 14:48:37 -0400

    Conditions Page and Footer Link
    Created conditions component, added to app.routing.module.ts, and updated 'Terms and Conditions' link on footer.

---
+++ Affordable-Housing/src/app/conditions/conditions.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'll-conditions',
+  templateUrl: './conditions.component.html',
+  styleUrls: ['./conditions.component.scss']
+})
+
+export class ConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
