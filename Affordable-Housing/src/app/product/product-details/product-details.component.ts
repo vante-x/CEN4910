3811ee5d342ed00605d5bced382cc80ad5392cad
Joanna Smith <joannanole@gmail.com>
2022-06-06 13:04:23 -0400

    Added google  map mockups for prototype presentation
    product-details page has a radio button list that will change the type of map displayed.

--- Affordable-Housing/src/app/product/product-details/product-details.component.ts
+++ Affordable-Housing/src/app/product/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'll-product-details',
@@ -8,25 +8,51 @@ import { NgForm } from '@angular/forms';
 })
 
 export class ProductDetailsComponent implements OnInit {
-  
-  mapsource: "/assets/images/google-maps/map_single_location3.jpg"
+    
+  title = 'app';
+  radioSel:any;
+  radioSelected:string;
+  radioSelectedString:string;
+  mapsource:string;
+
+  form = new FormGroup({
+    gender: new FormControl('')
+  });
+   
+  get f(){
+    return this.form.controls;
+  }
+   
+  submit(){
+    console.log(this.form.value);
+  }
+ 
+  changeMap(e) {
+
+    if(e.target.value == 'map')
+     { this.mapsource= "/assets/images/google-maps/map_single_location3.jpg";}
+     
+    if(e.target.value == 'shopping')
+    { this.mapsource= "/assets/images/google-maps/map_restaurant.jpg" ;}
+    
+    if(e.target.value == 'transit')
+     { this.mapsource= "/assets/images/google-maps/map_single_location3.jpg" ;}
+     
+    if(e.target.value == 'commute')
+    { this.mapsource= "/assets/images/google-maps/map_bus_route_to_work.jpg" ;}
+
+    if(e.target.value == 'schools')
+    { this.mapsource= "/assets/images/google-maps/map_schools.jpg" ;}
+  }
+
+   
   constructor() { 
-
+    this.mapsource= "/assets/images/google-maps/map_single_location3.jpg"
   }
-  onFormSubmit(form: NgForm) {
-
-    if(form.controls['options'].value=='transit'){
-      mapsource: "/assets/images/google-maps/map_single_location_bus_stops"
-
-    }
-
-  }
-
+  
   ngOnInit(): void {
     
   }
-  selectMap(e){
-    mapsource: "/assets/images/google-maps/map_single_location3.jpg"
-  }
+
 }
 
