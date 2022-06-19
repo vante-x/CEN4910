6e3960b91918ca536517089c00953d4f84a0c1ab
Alayan2 <90277439+Alayan2@users.noreply.github.com>
2022-06-19 10:41:56 -0400

    Revert "Merge pull request #52 from joannashad/41.2-home-page-changes"
    This reverts commit 19048499f17b71e1849b8715773582669a11843c, reversing
    changes made to 9a15870ca68777637106378e256224db638a176f.

--- Affordable-Housing/src/app/home/home.module.ts
+++ Affordable-Housing/src/app/home/home.module.ts
@@ -6,25 +6,9 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeProductsComponent } from './home-products/home-products.component';
 import { NgParticlesModule } from 'ng-particles';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { DataSource } from '@angular/cdk/collections';
-import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    HomeComponent, 
-    HomeProductsComponent, 
-  ],
-  imports: [CommonModule, HomeRoutingModule, SharedModule, 
-    NgParticlesModule, MatRadioModule, 
-    MatAutocompleteModule, 
-    ReactiveFormsModule, FormsModule, MatFormFieldModule, 
-    MatInputModule,
-    MatTableModule
-    ],
+  declarations: [HomeComponent, HomeProductsComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, NgParticlesModule]
 })
 export class HomeModule {}
